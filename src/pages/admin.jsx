ed78b317d17e0657fda654554365deb54d2298df
alishba2 <alishbashazadi79@gmail.com>
2024-10-03 00:31:46 +0500

    added language

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -12,28 +12,28 @@ export default function Admin() {
     const [isSidebarVisible, setIsSidebarVisible] = useState(true);
     const [isLoading, setIsLoading] = useState(true); // Add loading state for 30-second delay
     const navigate = useNavigate();
-    const { isAdmin } = useAdmin(); // Fetch isAdmin from context
+    // const { isAdmin } = useAdmin(); // Fetch isAdmin from context
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setIsLoading(false); // Stop loading after 30 seconds
-        }, 30000); // 30 seconds = 30000 ms
+    // useEffect(() => {
+    //     const timer = setTimeout(() => {
+    //         setIsLoading(false); // Stop loading after 30 seconds
+    //     }, 30000); // 30 seconds = 30000 ms
 
 
-        console.log(isAdmin, "isAdmin");
-        if (isAdmin) {
-            setIsLoading(false); // Stop loading immediately if admin status is already available
-        }
+    //     console.log(isAdmin, "isAdmin");
+    //     if (isAdmin) {
+    //         setIsLoading(false); // Stop loading immediately if admin status is already available
+    //     }
 
-        return () => clearTimeout(timer); // Clear the timer when the component unmounts
-    }, [isAdmin]);
+    //     return () => clearTimeout(timer); // Clear the timer when the component unmounts
+    // }, [isAdmin]);
 
-    useEffect(() => {
-        if (!isAdmin && !isLoading) {
-            // If not admin and loading has completed, redirect to home page
-            navigate('/');
-        }
-    }, [isAdmin, isLoading, navigate]);
+    // useEffect(() => {
+    //     if (!isAdmin && !isLoading) {
+    //         // If not admin and loading has completed, redirect to home page
+    //         navigate('/');
+    //     }
+    // }, [isAdmin, isLoading, navigate]);
 
     const renderComponent = () => {
         switch (activeComponent) {
@@ -47,15 +47,15 @@ export default function Admin() {
     };
 
     // Show loading spinner if isLoading is true
-    if (isLoading) {
-        return (
-            <div className="spinner-container">
-                <Spinner animation="border" role="status">
-                    {/* <span className="sr-only">Loading...</span> */}
-                </Spinner>
-            </div>
-        );
-    }
+    // if (isLoading) {
+    //     return (
+    //         <div className="spinner-container">
+    //             <Spinner animation="border" role="status">
+    //                 {/* <span className="sr-only">Loading...</span> */}
+    //             </Spinner>
+    //         </div>
+    //     );
+    // }
 
     return (
         <div className="admin-page">
